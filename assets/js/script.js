7473834cd8ec766ef9496de30308e1bd5010e9ee
Texier <nicolas.texier18@gmail.com>
2019-01-31 18:28:42 +0100

    Ajout du fichier CV.pdf

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+//Fonction pour le scroll
+window.onscroll = function() {scrollFunction(); navFunction()};
+function scrollFunction() {
+    //afficher ou masquer le topBtn en fonction du scroll
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+// retourne en haut de la page
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// Get the navbar
+var navbar = document.getElementById("navbar");
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function navFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+//function for CV PDF click
+function cvFunction(){
+    alert('Le CV PDF n\'est pas encore disponible');
+}
+//function for hire click
+function hireFunction(){
+    alert('Cette option n\'est pas encore disponible');
+}
+//function for contact click
+function contactFunction(){
+    alert('Cette option n\'est pas encore disponible');
+}
---
+++ readme.txt
@@ -0,0 +1,49 @@
+(4) : Section Formation à Crée
+
+(5) : Section Moi à finir
+
+(5.1) : Section Compétence à finir
+
+(5.1.1) : Btn Contact et Embauchez-moi
+
+(5.2) : Retirer les infos inutile du CV
+
+(6) : Crée un Footer
+
+(6.1) : arranger la navbar
+
+(7) : refaire son CV papier
+
+
+(5){
+    - Changement de la photo
+    - Remplir la présentation
+    - Ajout des hobbies (caroussel)https://getbootstrap.com/docs/4.2/components/carousel/1878×640
+}
+
+
+
+(5.1){
+    -Ajout des partie : éditeur de texte, versionning, framework, CMS
+}
+
+
+
+
+(5.1.1){
+  -Chercher "Fenetre modele"
+}
+
+
+
+(6){
+    -Logo gitHub, Linkedin
+    -Nom, Prénom
+    -© Texier Nicolas CV. All rights reserved.
+            Design - by Jonathan Marandeau
+}
+
+
+(6.1){
+  -petit décalage en mode sticky sur affichage tél
+}
