41c77cdba2d5521a55d4cbd912c148763f9e53a4
Texier <nicolas.texier18@gmail.com>
2019-02-07 16:29:41 +0100

    Problème navbar réglé + début de la section contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,50 @@
-//Fuction for scroll
-window.onscroll = function () {
-    scrollFunction();
-    navFunction()
-};
-function scrollFunction() {
-    //afficher ou masquer le topBtn en fonction du scroll
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("topBtn").style.display = "block";
-    } else {
-        document.getElementById("topBtn").style.display = "none";
+$(function () {
+    //Fuction for scroll
+    window.onscroll = function () {
+        scrollFunction();
+        navFunction()
+    };
+
+    function scrollFunction() {
+        //afficher ou masquer le topBtn en fonction du scroll
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("topBtn").style.display = "block";
+        } else {
+            document.getElementById("topBtn").style.display = "none";
+        }
     }
-}
-// return to Top
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-// Get the navbar
-var navbar = document.getElementById("navbar");
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function navFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
+    // return to Top
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
-}
-//function for CV PDF click
-function cvFunction() {
-    alert('Le CV PDF n\'est pas encore disponible');
-}
-//function for hire click
-function hireFunction() {
-    alert('Cette option n\'est pas encore disponible');
-}
-//function for contact click
-function contactFunction() {
-    alert('Cette option n\'est pas encore disponible');
-}
+    // Get the navbar
+    var navbar = document.getElementById("navbar");
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function navFunction() {
+        if (window.pageYOffset >= sticky) {
+            if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+                navbar.classList.add("sticky")
+            } else {
+                navbar.classList.remove("sticky");
+            }
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+    //function for CV PDF click
+    function cvFunction() {
+        alert('Le CV PDF n\'est pas encore disponible');
+    }
+    //function for hire click
+    function hireFunction() {
+        alert('Cette option n\'est pas encore disponible');
+    }
+
+    //function for contact click
+    $('#contactBtn').click(function(){
+        alert('test');
+    });
+});
--- readme.txt
+++ readme.txt
@@ -1,9 +1,7 @@
-(6) : Crée un Footer
+
 (5.1.1) : Btn Contact et Embauchez-moi
 
 (5.2) : Section Moi à finir
-
-(6.1) : arranger la navbar
 
 (7) : refaire son CV papier
 
@@ -21,13 +19,4 @@
 (5.2){
     - Changement de la photo
     - Remplir la présentation
-}
-
-
-(6.1){
-  -petit décalage en mode sticky sur affichage tél
-  
-  option1{
-    -essayer de mettre une fonction pour su le javascript ne s'active que à partie d'un certain moment et remettre des col(test possible sur le bouton top)
-  }
 }
